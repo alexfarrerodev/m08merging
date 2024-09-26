3a684d7009aa0f3ef058bc91d8e4ebab3e65e833
eblazqu3 <eblazqu3@xtec.cat>
2024-09-26 22:41:43 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        
+        // Operaciones básicas
+        System.out.println("Suma: " + calculadora.suma(5, 3));
+        System.out.println("Resta: " + calculadora.resta(5, 3));
+    }
+}
