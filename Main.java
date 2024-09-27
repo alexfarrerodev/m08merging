dbfab43e54317fee520567a3209dc485d34b1b5d
cacara82 <cacara890@gmail.com>
2024-09-27 20:30:13 +0200

    commit 3

--- Main.java
+++ Main.java
@@ -5,5 +5,11 @@ public class Main {
         // Operaciones básicas
         System.out.println("Suma: " + calculadora.suma(5, 3));
         System.out.println("Resta: " + calculadora.resta(5, 3));
-    }
+	
+	if (Utilidades.esPositivo(5)) { 
+System.out.println("El número es positivo");
+ }
+    
+}
+
 }
